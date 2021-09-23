7fc1a4cfbb93e76379f576d0b50ac49d3417ff5a
albert <albertperezdatsira@gmail.com>
2021-09-23 19:58:31 +0200

    Create hello-parallel-world.c

---
+++ OpenMP/hello-parallel-world.c
@@ -0,0 +1,13 @@
+#include <omp.h>
+#include <stdio.h>
+ 
+int main ()  
+{
+   #pragma omp parallel
+   {
+      int id = omp_get_thread_num();
+      printf("Hello World from thread = %d of %d threads.\n", id, omp_get_num_threads());
+   }
+
+   return 0;
+}
